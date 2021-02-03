5af47f7fc2137327cd5afc1e1156de3a504779bc
rafsalas19 <rafael@3e9b17ecafd5>
2021-02-03 02:33:15 +0000

    Script to create an NV windows machine (w/o ext)

---
+++ az_cli/provision_NV_GPU_EXT/nvExtScript.sh
@@ -0,0 +1,23 @@
+az account set --subscription "Visual Studio Enterprise Subscription"
+
+# You can check what sizes are available on based on subscription and location
+#az vm list-sizes --location southcentralus --subscription "Visual Studio Enterprise Subscription" | grep -i name | grep -i NV
+
+SIZE=Standard_NV6_Promo
+RESOURCEGROUP=nvResourceGroup
+LOCATION=southcentralus
+VM_NAME=NVVM
+#find images with offering Windows-10
+# az vm image list --offer Windows-10 --all --output table
+
+IMAGE="MicrosoftWindowsDesktop:Windows-10:20h2-ent:latest"
+USERNAME=rafsalas
+az group create --name $RESOURCEGROUP  --location $LOCATION
+
+az vm create --name $VM_NAME \
+             --resource-group $RESOURCEGROUP \
+             --size $SIZE\
+             --image $IMAGE\
+             --admin-username $USERNAME 
+            #  --admin-password \
+            
