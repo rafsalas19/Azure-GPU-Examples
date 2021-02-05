a8809bb903c094f5731bf2547db7e148ddd37ab1
rafsalas19 <rafsalas1987@hotmail.com>
2021-02-05 14:04:10 +0000

    fixed bug with sshkey now the nasible scriptworks

--- ansible/ncExtAnsible.sh
+++ ansible/ncExtAnsible.sh
@@ -4,4 +4,4 @@ export AZURE_SUBSCRIPTION_ID=`az account show | grep id | awk -F\" '{print $4}' 
 
 SSHKEY=`cat ~/.ssh/id_rsa.pub`
 #echo $SSHKEY
-ansible-playbook nvExtAnsible.yml --extra-vars "name=rafTest location=eastus size=Standard_NC6_Promo userName=rafsalas sshKey=$SSHKEY"
+ansible-playbook ncExtAnsible.yml --extra-vars "name=rafTest location=eastus size=Standard_NC6_Promo userName=rafsalas sshKey='${SSHKEY}'"
